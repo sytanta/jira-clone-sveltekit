e186f4d9a0557e02626eefe26ae5f6a5f5c4f196
TA SY TAN <sytanta@yahoo.com>
2023-02-03 19:27:38 +0700

    Remove console log

--- src/routes/api/auth/create-guest-account/+server.ts
+++ src/routes/api/auth/create-guest-account/+server.ts
@@ -6,7 +6,7 @@ import { signToken, tokenCookiesOptions } from '$lib/utils/auth';
 
 export const POST: RequestHandler = async function ({ cookies }) {
 	const { data: user, error } = await asyncRequest(createGuestAccount);
-	console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzz', error);
+
 	if (!user) throw error(500, { message: 'Error creating guest account' });
 
 	const token = signToken({ id: user.id });
